94e7b518f3296abd17255f01242b533056c6de4e
Josh Stickles <sticklesjb@gmail.com>
2014-02-28 08:39:15 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+	var body = 'Hello World';
+	res.setHeader('Content-Type', 'text/plain');
+	res.setHeader('Content-Length', body.length);
+	res.end(body);
+});
+
+app.post('/update', function( req, res ) {
+
+});
+
+app.listen( process.env.PORT || 3000);
