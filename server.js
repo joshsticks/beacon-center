650c68f1e63887a5d16aecfd6825f0b6db50c45d
Josh Stickles <sticklesjb@gmail.com>
2014-02-28 08:56:20 -0500

    added in stupid mongodb stuff

--- server.js
+++ server.js
@@ -5,6 +5,18 @@ app.get('/', function(req, res){
 	var body = 'Hello World';
 	res.setHeader('Content-Type', 'text/plain');
 	res.setHeader('Content-Length', body.length);
+	var mongo = require('mongodb');
+
+	var mongoUri = process.env.MONGOLAB_URI ||
+  			  process.env.MONGOHQ_URL ||
+  			  'mongodb://test:pancakes@ds033489.mongolab.com:33489/heroku_app22618166';
+
+	mongo.Db.connect(mongoUri, function (err, db) {
+  		db.collection('mydocs', function(er, collection) {
+    			collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
+    			});
+  		});
+	});
 	res.end(body);
 });
 
